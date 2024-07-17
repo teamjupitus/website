bdda4309c348919b4276b7e4f88064a897c435c0
teamjupitus <teamjupitus@gmail.com>
2024-07-17 20:11:30 +0530

    First Upload

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// Smooth scrolling for navbar links
+$(document).ready(function(){
+    $(".scrollto").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+});
